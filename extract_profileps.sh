ad791fbcc1ad7449b4c38f4cce95a2eeccb5149e
salesforcedoc <1036960+salesforcedoc@users.noreply.github.com>
2019-05-30 11:08:28 -0400

    update documentation/comments

--- extract_profileps.sh
+++ extract_profileps.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#
+# usage: extract_profileps.sh <sfdx_org_alias>
+# description: extracts all profiles/permsets from <sfdx_org_alias> and outputs to a folder named 'src/<sfdc_org_alias>'
+#
+# extract_profile.sh - example script to extract profiles and persmission sets from an org
+# extract_profile.awk - example awk script used to generate the package.xml
+#
+# this script requires you to connect the sfdx cli to an org.  see below for examples:
+#
+# sfdx force:auth:web:login -r https://test.salesforce.com -a <sfdx_org_alias>
+# sfdx force:auth:web:login -r https://login.salesforce.com -a <sfdx_org_alias>
+# 
+
+# function to check and create a directory
+chkmkdir() {
+    local directory_name
+    directory_name="$1" 
+    if [ ! -d "${directory_name}" ]; then
+        mkdir -p "${directory_name}"
+    fi
+}
+
 # check for sfdx first
 which sfdx >/dev/null
 if [ $? -eq 1 ]; then
@@ -7,12 +29,9 @@ if [ $? -eq 1 ]; then
    exit 1
 fi
 
-# connect sfdx cli to an org and use the alias here
-# use sfdx force:auth:web:login -r https://test.salesforce.com -a <alias>
-
-# check for an org_alias
+# check for an sfdx org alias
 if [ -z "$1" ]; then
-    echo "Please specify an alias. $0 <org_alias>"
+    echo "Please specify an sfdx org alias. $0 <sfdx_org_alias>"
     exit 1
 fi
 
@@ -21,19 +40,15 @@ SFDX_ORG_ALIAS=$1
 TGT_DIR=./src/${SFDX_ORG_ALIAS}
 TMP_DIR=./tmp/${SFDX_ORG_ALIAS}
 
-# check and create the directory
-chkmkdir() {
-    local directory_name
-    directory_name="$@" 
-    if [ ! -d "${directory_name}" ]; then
-        mkdir -p "${directory_name}"
-    fi
-}
-
 # create the target directory
 chkmkdir ${TGT_DIR}/profiles
-chkmkdir ${TGT_DIR}/permissionsets 
+chkmkdir ${TGT_DIR}/permissionsets
 chkmkdir ${TMP_DIR}
+
+# cleanup previous files
+rm -rf "${TGT_DIR}/profiles/*"
+rm -rf "${TGT_DIR}/permissionsets/*"
+rm -rf "${TMP_DIR}/*"
 
 # generate a list of sobjects and create the package.xml
 echo Creating sobject/profile/permsets package.xml...
@@ -57,7 +72,7 @@ if [ -f "${TMP_DIR}/unpackaged.zip" ]; then
     cp -r ${TMP_DIR}/unpackaged/package.xml "${TGT_DIR}"
     echo \* | awk -f extract_profileps.awk > ${TGT_DIR}/package.xml
 
-    # cleanup
+    # cleanup - uncomment the below if you want to have it cleanup the temp directory
     #echo Cleaning up...
     #rm -rf "${TMP_DIR}/*"
 
